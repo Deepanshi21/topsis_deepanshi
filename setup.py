85eb3f900e5ed647d187937aea4e908b74b4eb98
Deepanshi21 <79203522+Deepanshi21@users.noreply.github.com>
2022-02-27 17:49:01 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsis-deepanshi-401903006",
+    version="1.0.0",
+    author="Deepanshi Srivastava",
+    author_email="dsrivastava_bemba19@thapar.edu",
+    description="A package -> Calculates Topsis Score and Rank accordingly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/Deepanshi21/topsis_deepanshi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["topsis_deepanshi"],
+    include_package_data=True,
+    install_requires='pandas',
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_deepanshi.topsis:main",
+        ]
+    },
+)
